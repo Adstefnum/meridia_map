3c5378ab8ecac44a901f337f7a4dee7f585897d5
Adstefnum <bauerstevenson@gmail.com>
2022-01-13 14:04:16 +0200

    hmmn

--- app/src/main/java/com/example/meridia_map/MapModel.kt
+++ app/src/main/java/com/example/meridia_map/MapModel.kt
@@ -1,6 +1,19 @@
 package com.example.meridia_map
 
 
+val json    = Json(JsonConfiguration.Stable)
+val mapmodel = json.parse(MapModel.serializer(), jsonString)
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+@Serializable
+data class MapModel (
+    val features: List<Feature>
+)
+
 @Serializable
 data class Feature (
     val id: Long,
