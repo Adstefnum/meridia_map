fefd5b221b8a1697c2f047314d0e63451f04599c
Adstefnum <bauerstevenson@gmail.com>
2022-01-13 13:36:11 +0200

    setting up data querying

--- app/src/main/java/com/example/meridia_map/MapModel.kt
+++ app/src/main/java/com/example/meridia_map/MapModel.kt
@@ -1,8 +1,13 @@
+package com.example.meridia_map
+
+
+@Serializable
 data class Feature (
     val id: Long,
     val points: List<Point>
 )
 
+@Serializable
 data class Point (
     val accuracy: Double,
     val latitude: Double,
---
+++ app/src/main/java/com/example/meridia_map/MapRepository.kt
@@ -0,0 +1,17 @@
+package com.example.meridia_map
+
+import com.google.android.libraries.maps.model.LatLng
+
+
+class MapRepository {
+
+ fun getMapData(): List<MapModel> {
+
+        return listOf(
+
+        		val myThing: MyThing = Json.parse(MyThing.serializer(), myThingInJsonText)
+
+
+        	)
+    }
+}
